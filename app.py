513a3ce927c52f7ecd7e3adb87d7c86af9d8aff5
Andrei <andrei.oprica99@e-uvt.ro>
2022-05-03 18:08:33 +0300

    05.03.2022 - v2

--- app.py
+++ app.py
@@ -19,8 +19,7 @@ def compute_tf(word_dict, bow):
 def compute_idf(doc_list):
 
     import math
-    idf_dict = {}
-    N = len(doc_list)
+    n = len(doc_list)
     idf_dict = dict.fromkeys(doc_list[0].keys(), 0)
 
     for doc in doc_list:
@@ -29,7 +28,7 @@ def compute_idf(doc_list):
                 idf_dict[word] += 1
 
     for word, val in idf_dict.items():
-        idf_dict[word] = math.log10(N / float(val))
+        idf_dict[word] = math.log10(n / float(val))
 
     return idf_dict
 
@@ -57,6 +56,7 @@ def computer_science():
     cur = conn.cursor()
     cur.execute('SELECT * FROM ComputerScience')
 
+    c0 = []
     c1 = []
     c2 = []
     c3 = []
@@ -66,35 +66,22 @@ def computer_science():
     c7 = []
     c8 = []
     c9 = []
-    c10 = []
-    c11 = []
-    c12 = []
-    c13 = []
-    c14 = []
-    c15 = []
-    c20 = []
 
     for row in cur:
         c1.append(row[0])
         c2.append(row[1])
-        c3.append(row[2])
-        c4.append(row[3])
-        c5.append(row[4])
-        c6.append(row[5])
-        c7.append(row[6])
-        c8.append(row[7])
-        c9.append(row[8])
-        c10.append(row[9])
-        c11.append(row[10])
-        c12.append(row[11])
-        c13.append(row[12])
-        c14.append(row[13])
-        c15.append(row[14])
+        c3.append(row[3])
+        c4.append(row[5])
+        c5.append(row[7])
+        c6.append(row[9])
+        c7.append(row[11])
+        c8.append(row[12])
+        c9.append(row[14])
 
     # calculate score and add it to lists
-    for i in range(len(c13)):
-        bow_category = c13[i].split(" ")
-        bow_description = c15[i].split(" ")
+    for i in range(len(c8)):
+        bow_category = c8[i].split(" ")
+        bow_description = c9[i].split(" ")
 
         word_set = set(bow_category).union(set(bow_description))
 
@@ -115,15 +102,15 @@ def computer_science():
         tfidf_bow_category = compute_tfidf(tf_bow_category, idfs)
         tfidf_bow_description = compute_tfidf(tf_bow_description, idfs)
 
-        c20.append((tfidf_bow_category + tfidf_bow_description))
+        c0.append((tfidf_bow_category + tfidf_bow_description))
 
-    for i in range(len(c20)):
+    for i in range(len(c0)):
         max_score = i
-        for j in range(i+1, len(c20)):
-            if c20[max_score] < c20[j]:
+        for j in range(i+1, len(c0)):
+            if c0[max_score] < c0[j]:
                 max_score = j
 
-        c20[i], c20[max_score] = c20[max_score], c20[i]
+        c0[i], c0[max_score] = c0[max_score], c0[i]
         c1[i], c1[max_score] = c1[max_score], c1[i]
         c2[i], c2[max_score] = c2[max_score], c2[i]
         c3[i], c3[max_score] = c3[max_score], c3[i]
@@ -133,15 +120,9 @@ def computer_science():
         c7[i], c7[max_score] = c7[max_score], c7[i]
         c8[i], c8[max_score] = c8[max_score], c8[i]
         c9[i], c9[max_score] = c9[max_score], c9[i]
-        c10[i], c10[max_score] = c10[max_score], c10[i]
-        c11[i], c11[max_score] = c11[max_score], c11[i]
-        c12[i], c12[max_score] = c12[max_score], c12[i]
-        c13[i], c13[max_score] = c13[max_score], c13[i]
-        c14[i], c14[max_score] = c14[max_score], c14[i]
-        c15[i], c15[max_score] = c15[max_score], c15[i]
 
-    for i in range(len(c20)):
-        if c15[i] == '':
+    for i in range(len(c0)):
+        if c9[i] == '':
             del c1[i]
             del c2[i]
             del c3[i]
@@ -151,19 +132,15 @@ def computer_science():
             del c7[i]
             del c8[i]
             del c9[i]
-            del c10[i]
-            del c11[i]
-            del c12[i]
-            del c13[i]
-            del c14[i]
-            del c15[i]
 
     # put data in a dictionary and return data to a template
 
     output = {}
 
     for i in range(len(c1)):
-        lst = [c1[i], c2[i], c3[i], c4[i], c5[i], c6[i], c7[i], c8[i], c9[i], c10[i], c11[i], c12[i], c13[i], c15[i], c20[i]]
+        lst = [c1[i], c8[i], c9[i], c2[i], c3[i], c4[i], c5[i], c6[i], c7[i]]
+        # to print the scores uncomment the next line:
+        # lst.append(c0[i])
         output[i] = copy.copy(lst)
 
     return render_template('computerScience.html', output=output.items())
@@ -177,6 +154,7 @@ def machine_learning():
     cur = conn.cursor()
     cur.execute('SELECT * FROM MachineLearning')
 
+    c0 = []
     c1 = []
     c2 = []
     c3 = []
@@ -186,35 +164,22 @@ def machine_learning():
     c7 = []
     c8 = []
     c9 = []
-    c10 = []
-    c11 = []
-    c12 = []
-    c13 = []
-    c14 = []
-    c15 = []
-    c20 = []
 
     for row in cur:
         c1.append(row[0])
         c2.append(row[1])
-        c3.append(row[2])
-        c4.append(row[3])
-        c5.append(row[4])
-        c6.append(row[5])
-        c7.append(row[6])
-        c8.append(row[7])
-        c9.append(row[8])
-        c10.append(row[9])
-        c11.append(row[10])
-        c12.append(row[11])
-        c13.append(row[12])
-        c14.append(row[13])
-        c15.append(row[14])
+        c3.append(row[3])
+        c4.append(row[5])
+        c5.append(row[7])
+        c6.append(row[9])
+        c7.append(row[11])
+        c8.append(row[12])
+        c9.append(row[14])
 
     # calculate score and add it to lists
-    for i in range(len(c13)):
-        bow_category = c13[i].split(" ")
-        bow_description = c15[i].split(" ")
+    for i in range(len(c8)):
+        bow_category = c8[i].split(" ")
+        bow_description = c9[i].split(" ")
 
         word_set = set(bow_category).union(set(bow_description))
 
@@ -235,15 +200,15 @@ def machine_learning():
         tfidf_bow_category = compute_tfidf(tf_bow_category, idfs)
         tfidf_bow_description = compute_tfidf(tf_bow_description, idfs)
 
-        c20.append((tfidf_bow_category + tfidf_bow_description))
+        c0.append((tfidf_bow_category + tfidf_bow_description))
 
-    for i in range(len(c20)):
+    for i in range(len(c0)):
         max_score = i
-        for j in range(i + 1, len(c20)):
-            if c20[max_score] < c20[j]:
+        for j in range(i+1, len(c0)):
+            if c0[max_score] < c0[j]:
                 max_score = j
 
-        c20[i], c20[max_score] = c20[max_score], c20[i]
+        c0[i], c0[max_score] = c0[max_score], c0[i]
         c1[i], c1[max_score] = c1[max_score], c1[i]
         c2[i], c2[max_score] = c2[max_score], c2[i]
         c3[i], c3[max_score] = c3[max_score], c3[i]
@@ -253,15 +218,9 @@ def machine_learning():
         c7[i], c7[max_score] = c7[max_score], c7[i]
         c8[i], c8[max_score] = c8[max_score], c8[i]
         c9[i], c9[max_score] = c9[max_score], c9[i]
-        c10[i], c10[max_score] = c10[max_score], c10[i]
-        c11[i], c11[max_score] = c11[max_score], c11[i]
-        c12[i], c12[max_score] = c12[max_score], c12[i]
-        c13[i], c13[max_score] = c13[max_score], c13[i]
-        c14[i], c14[max_score] = c14[max_score], c14[i]
-        c15[i], c15[max_score] = c15[max_score], c15[i]
 
-    for i in range(len(c20)):
-        if c15[i] == '':
+    for i in range(len(c0)):
+        if c9[i] == '':
             del c1[i]
             del c2[i]
             del c3[i]
@@ -271,19 +230,15 @@ def machine_learning():
             del c7[i]
             del c8[i]
             del c9[i]
-            del c10[i]
-            del c11[i]
-            del c12[i]
-            del c13[i]
-            del c14[i]
-            del c15[i]
 
     # put data in a dictionary and return data to a template
 
     output = {}
 
     for i in range(len(c1)):
-        lst = [c1[i], c2[i], c3[i], c4[i], c5[i], c6[i], c7[i], c8[i], c9[i], c10[i], c11[i], c12[i], c13[i], c15[i], c20[i]]
+        lst = [c1[i], c8[i], c9[i], c2[i], c3[i], c4[i], c5[i], c6[i], c7[i]]
+        # to print the scores uncomment the next line:
+        # lst.append(c0[i])
         output[i] = copy.copy(lst)
 
     return render_template('machineLearning.html', output=output.items())
